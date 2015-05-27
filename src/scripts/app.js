ad442773cd3e0035b771c4f60d44c6349ed7efa3
Abraham José González Barboza <jgab.net@gmail.com>
2015-05-27 00:34:18 -0430

    agregado de items

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,30 +2,34 @@
 
 angular
   .module('demo', ['awesome'])
-  .controller('MainController', function (Dictionary) {
+  .controller('MainController', function ($scope, $timeout, Dictionary) {
 
     this.list = Dictionary.query();
 
-    this.test = function () {
-      this.list.push ({
-        "_id":"5532fdef4a8f7cd53c0fdc35",
-        "parent":"5532fdef4a8f7cd53c0fdc33",
-        "label":"TEST TEST TEST",
-        "name":"test",
-        "__v":0,
-        "items":[],
-        "flags":{
-          "visible":true,
-          "value":false,
-          "end":false
-        },
-        "type":"string",
-        "created":"2015-04-19T00:59:27.000Z",
-        "middle":false,
-        "leaf":true,
-        "root":false,
-        "id":"5532fdef4a8f7cd53c0fdc35"
-      });
+    this.getItem = function (item, father) {
 
-    }.bind(this);
+      $timeout(function () {
+        var newItem = {
+          "_id":"5532fdef4a8f7cd53c0fd312",
+          label: item,
+          name: item,
+          "items":[],
+          "flags":{
+            "visible":true,
+            "value":false,
+            "end":false
+          },
+          "type":"string",
+          "created":"2015-04-19T00:59:27.000Z",
+          "middle":false,
+          "leaf":true,
+          "root":false,
+          parent: father._id
+        };
+
+        $scope.awesome.addItem(newItem);
+      }, 500);
+
+    };
+
   });
--- src/scripts/awesome.js
+++ src/scripts/awesome.js
@@ -19,14 +19,17 @@ angular
         filter: '@',
         childrens: '@',
         cacheKey:'@',
-        limit:'='
+        getItem:'&?',
+        limit:'=',
+        alias:'@'
       },
-      controller: function ($scope) {
+      controller: function ($scope, $element) {
         this.filter = this.filter || 'label';
         this.placeholder = this.placeholder || 'search...';
         this.select = 0;
         this.cacheKey = this.cacheKey || '_id';
         this.show = false;
+        this.father = undefined;
 
         var expression = this.base.match( /^\s*(\w+)\s+in\s+([\w.]+)\s*$/i );
         if (!expression) {
@@ -39,23 +42,111 @@ angular
           list: AwesomeService.flatTree($parse(expression[2])($scope.$parent))
         }];
 
-        this.active = function (event) {
+        this.active = function () {
           if (this.suggestions[this.select] &&
               this.suggestions[this.select][this.childrens]) {
-            if(event) event.preventDefault();
             this.list.push({
               name: this.suggestions[this.select][this.filter],
+              item: this.suggestions[this.select],
               list: this.suggestions[this.select][this.childrens]
             });
             return true;
           }
           return false;
         };
+
+        this.preAddItem = function (text) {
+          if(this.list.length > 1 && !this.list[this.list.length-1].add ) {
+            this.list.push({
+              name: text,
+              list: [],
+              add: true
+            });
+            return true;
+          }
+          return false;
+        };
+
+        this.deductFather = function () {
+          var father = undefined;
+          if (this.list.length > 1) {
+
+            var child = undefined;
+            for (var i = this.list.length - 1; i >= 0; i--) {
+              if (this.list[i].item) {
+                if (this.list[i].item[this.childrens].length > 0) {
+                  father = this.list[i].item;
+                  break;
+                } else {
+                  child = child || this.list[i].item;
+                }
+              }
+            }
+            if (!father) {
+              father = this.list[0].list.filter(function (item) {
+                return item[this.cacheKey] = child.parent;
+              }.bind(this))[0];
+            }
+          }
+          return father;
+        };
+
+        this.requestItem = function () {
+          this.getItem({
+            name: this.list[this.list.length-1].name,
+            father: this.father
+          });
+        };
+
+        this.addItem = function (item) {
+          this.list[0].list.push(item);
+          if (this.father) {
+            this.father[this.childrens].push(item);
+            this.father = undefined;
+          } else {
+            this.list[0].list.filter(function (item) {
+              return item[this.cacheKey] = child.parent;
+            }.bind(this))[0][this.childrens].push(item);
+          }
+        };
+
+        this.extern = {
+          addItem: function (item) {
+            this.addItem(item);
+            if (this.list.length >= 2) {
+              for (var i = this.list.length - 1; i >= 0; i--) {
+                if (this.list[i].item) {
+                  console.log(this.list[i].item);
+                  this.list.splice(i, this.list.length-i, {
+                    name: item.label,
+                    item: item,
+                    list: []
+                  });
+                  break;
+                }
+              }
+            }
+
+            $element.find('#preAdd').data('add', true).modal('hide');
+
+          }.bind(this)
+        };
       },
-      link: function (scope, element, attrs, ngModel, $transclude) {
+      link: function (scope, element, attr, ngModel, $transclude) {
+        if (scope.awesome.alias)
+          scope.$parent[scope.awesome.alias] = scope.awesome.extern || {};
+
         var $input = element.find('.aw-input');
         var $placeholder = element.find('.aw-placeholder');
         var $list = element.find('.aw-list');
+        var $modal = element.find('#preAdd');
+
+        scope.awesome.cancelFilter = function (index) {
+          scope.awesome.list.splice(index, this.list.length-index);
+          $timeout(function () {
+            $input.focus();
+          });
+        };
 
         scope.$watchCollection('awesome.suggestions', function (collection) {
           if (!collection) return;
@@ -99,15 +190,34 @@ angular
           scope.$apply();
         });
 
+        $list.on('mouseenter', function () {
+          scope.awesome.hover = true;
+          console.log(scope.awesome.hover);
+        });
+
+        $list.on('mouseleave', function () {
+          scope.awesome.hover = false;
+          console.log(scope.awesome.hover);
+        });
+
         $input.on('keydown', function (event) {
           var keyCode = event.which || event.keyCode;
 
           if ((keyCode == 13 || keyCode == 9)) {
-            if (scope.awesome.active(event)) {
+            if (scope.awesome.active()) {
+              event.preventDefault();
               $input.html('');
-              $timeout(function () {
+              /*$timeout(function () {
                 $input.focus();
-              });
+              });*/
+            } else if($input.html() !== '' && angular.isUndefined(attr.getItem) === false) {
+              if (scope.awesome.preAddItem($input.html())) {
+                event.preventDefault();
+                $input.html('');
+                $modal.modal('show');
+              }
+            } else if(keyCode == 13) {
+              event.preventDefault();
             }
           }
 
@@ -149,8 +259,26 @@ angular
           if ($input.html() === '') {
             $input.append($placeholder);
           }
-          scope.awesome.show = false;
-          scope.$apply();
+          if (!scope.awesome.hover) {
+            scope.awesome.show = false;
+            scope.$apply();
+          }
+        });
+
+        $modal.on('hide.bs.modal', function () {
+
+          if ($modal.data('add') === false) {
+            scope.awesome.list.splice(scope.awesome.list.length -1, 1);
+            scope.awesome.father = undefined;
+          }
+          $modal.data('add', false);
+          $timeout(function () {
+            $input.focus();
+          });
+        });
+
+        $modal.on('show.bs.modal', function () {
+          scope.awesome.father = scope.awesome.deductFather();
         });
 
         scope.$watch('awesome.show', function () {
